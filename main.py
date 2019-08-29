a31a39d4db145f07ee705e8c096df911e52dc14e
Vincent Lemieux <vincent.lemieux0599@gmail.com>
2019-08-28 20:47:26 -0400

    Prints unprinted pdfs in notes subdirectory of each class folder, then adds their absolute path to list of printed documents

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
 import os, sys
-import win32print
+import win32print, win32api
+import datetime as dt
+win32print.SetDefaultPrinter(win32print.GetDefaultPrinter())
+
+#Gets list of already printed documents from printed.txt
+printed_docs = []
+f= open("printed.txt", "a")
+f1 = open ("printed.txt", "r")
+print ("already printed documents: ")
+for line in f1.readlines():
+    if line.startswith("C"):
+        print (line)
+        printed_docs.append(line.replace("\n", ""))
+f1.close()
+
+
+#Gets date and adds path according to semester and year (FORMAT :  "\SEMESTER YEAR")
+def findpath():
+    print("------------------BEGIN findpath function-------------")
+    path = "C:\\Users\\vince\\Google Drive\\Mcgill"
+    print(path.endswith("Mcgill"))
+    date = dt.date.today()
+    if date.month > 6 and path.endswith("Mcgill"):
+        path += "\\Fall " + str(date.year)
+        print("Fall " , date.year)
+        print (path)
+    elif date.month <=6:
+        path += "\\Winter " + str(date.year)
+        print("Winter", date.year)
+        print (path)
+    print("---------------END findpath function--------------------")
+    return path
+
+
+for dir in os.listdir(findpath()):
+    path = findpath()
+    print ("dir",dir)
+    path += "\\%s" %dir + "\\notes"
+    for file in os.listdir(path):
+        file_path = path + "\\%s" %file
+        if file.endswith(".pdf") and file_path not in printed_docs:
+            print (file_path)
+            printed_docs.append(file_path)
+            f.write("\n"+ file_path)
+            win32api.ShellExecute(0, "print",file_path, None, ".", 0)
+
+f.close()
